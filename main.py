04b56f8480c9501833553a30b46781463927dc7a
Akarsh Pandey <akarshpandey9599@gmail.com>
2022-06-30 00:02:45 +0530

    added api auto complete

--- main.py
+++ main.py
@@ -1,9 +1,54 @@
+import uvicorn
+import pymongo
+from bson.objectid import ObjectId
+
+client = pymongo.MongoClient("mongodb+srv://partnersInCrime:partners123@cluster0.grt0lph.mongodb.net/?retryWrites=true&w=majority")
+db = client["partnersInCrime"]
 from fastapi import FastAPI
 
-# from .db import read, read_one, create, update, delete 
+#from .db import read, read_one, create, update, delete 
 
 app = FastAPI()
+@app.get("/suggestions")
+def autocomp(q):
+    pipeline = [
+   {
+     '$search': {
+       'index': 'autodefault',
+       "autocomplete": {
+         "query": q,
+         "path": "name",
+         "tokenOrder": "sequential"
+       }
+     }
+   },
+   {
+     '$limit': 10
+   },
+   {
+     '$project': {
+       "name": 1
+     }
+   }
+    ]
+    count=0
+
+    collections=db["users"]
+    aggregatedresult=collections.aggregate(pipeline)
+    result={}
+    data=[]
+    for i in list(aggregatedresult):
+        count+=1
+        data.append({"name":i["name"]})
+    result["meta"]={"total":count}
+    result["data"]=data
+
+    return result
+
+
 
 @app.get("/")
 def home():
     return {"Let's": "Go"}
+if __name__ == '__main__':
+    uvicorn.run(app, port=8000)
