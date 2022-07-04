fdaba5961856175ad6f1c77a470fed5fd3e7c5f8
bhavyag54 <bhavyagoyal54@gmail.com>
2022-07-04 17:41:37 +0530

    updated auth

--- auth.py
+++ auth.py
@@ -1,7 +1,7 @@
 from typing import Union
 from fastapi import  Header, APIRouter, Request
-# from google.oauth2 import id_token
-# from google.auth.transport import requests
+from google.oauth2 import id_token
+from google.auth.transport import requests
 from pydantic import BaseModel
 import json
 from db import db, read_one, create
@@ -11,9 +11,9 @@ router = APIRouter()
 def verify(authorization):
     auth_token = authorization.split(" ")[1]
     try:
-        user = id_token.verify_oauth2_token(auth_token,requests.Request(),  '712712296189-2oahq4t0sis03q14jqoccs8e6tuvpbfd.apps.googleusercontent.com', clock_skew_in_seconds=10)
+        user = id_token.verify_firebase_token(auth_token,requests.Request(),  '712712296189-2oahq4t0sis03q14jqoccs8e6tuvpbfd.apps.googleusercontent.com', clock_skew_in_seconds=10)
         print(user)
-        return user
+        return True
     except:
         return False
 
